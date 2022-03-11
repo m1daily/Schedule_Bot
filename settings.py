5b640fa3d52e045cad9ce347acfd6d8a2d28efb0
Geusen <89836078+Geusen@users.noreply.github.com>
2022-03-11 17:03:33 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-CK = os.environ.get("CONSUMER_KEY") # 環境変数の値をCKに代入
+CK = os.environ.get("CONSUMER_KEY") # 環境変数の値をCKに代入(以下同様)
 CS = os.environ.get("CONSUMER_SECRET")
 AT = os.environ.get("ACCESS_TOKEN")
 ATC = os.environ.get("ACCESS_TOKEN_SECRET")
 GU = os.environ.get("GOOGLE_URL")
 EM = os.environ.get("E_MAIL")
 PW = os.environ.get("PASSWORD")
+LN = os.environ.get("LINE_NOTIFY")
