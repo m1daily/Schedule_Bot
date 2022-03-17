654a5c7121d65040bfcaad866600d38b01f3ee88
Geusen <89836078+Geusen@users.noreply.github.com>
2022-03-18 08:39:40 +0900

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -85,18 +85,18 @@ im = Image.open('before.png')
 im.crop((35, 145, 640, 645)).save('now.png', quality=95)
 #-----------------------------------------------------------------------------
 #Megaにログイン(e-mailとパスワードは伏せています)
-mega = Mega()
-email = settings.EM
-password = settings.PW
-m = mega.login(email,password)
+#mega = Mega()
+#email = settings.EM
+#password = settings.PW
+#m = mega.login(email,password)
 
 #画像取得
-file = m.find('upload.png')
-m.download(file)
+#file = m.find('upload.png')
+#m.download(file)
 #-----------------------------------------------------------------------------
 #画像比較
-img_1 = cv2.imread('now.png')
-img_2 = cv2.imread('upload.png')
+#img_1 = cv2.imread('now.png')
+#img_2 = cv2.imread('upload.png')
 
 #もしスクショした画像とアップロード済みの画像が異なる(＝時間割が更新された)なら
 #if np.array_equal(img_1, img_2) == False:
