d63bb3b428c722efaa4efdd3c23f6a6cd9fd2750
Geusen <89836078+Geusen@users.noreply.github.com>
2022-03-18 09:01:45 +0900

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -88,7 +88,7 @@ im.crop((35, 145, 640, 645)).save('now.png', quality=95)
 mega = Mega()
 email = settings.EM
 password = settings.PW
-m = mega.login(email,password)
+#m = mega.login(email,password)
 
 #画像取得
 #file = m.find('upload.png')
