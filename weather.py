25aabd15155b57ff2987ddb55f4404e71ba1295e
Geusen <89836078+Geusen@users.noreply.github.com>
2022-05-12 18:48:25 +0900

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,10 +1,8 @@
 import settings
-import request
 import tweepy
 import time
 import datetime
 from PIL import Image
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
