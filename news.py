0a1406a11d253ec20c4ab3e2046f9d9f85fb53f3
Geusen <89836078+Geusen@users.noreply.github.com>
2022-08-21 21:50:14 +0900

    Create news.py

---
+++ news.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image, ImageDraw, ImageFont
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# ユーザーエージェントを変更し、403エラー対策
+ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+headers = {'User-Agent': ua}
+
+# 月間予定のタグを抽出
+url = "https://www.mito1-h.ibk.ed.jp/"
+r = requests.get(url, headers=headers)
+soup = BeautifulSoup(r.text, 'html.parser')
+ht = soup.select_one('#box-18 > section:nth-child(3) > div.panel-body.block > article > p')
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# 余計な文字列を削除
+ht = [i.strip() for i in ht.text.splitlines()]
+ht = [i for i in ht if i != ""]
+ht = ht[0].split()
+li = []
+for i in ht:
+    a = ''.join(i.split())
+    table = str.maketrans('（）', '()')
+    a = a.translate(table)
+    if a[0].isdecimal() == True:
+        a = "\n" + a
+    elif a[0] == "(":
+        a = a
+    else:
+        a = "\n                " + a
+    li.append(a)
+# リスト結合
+li = ''.join(li)
+print(li)
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# 画像に文字を入れる
+im = Image.new("RGB", (512, 680), (256, 256, 256))
+draw = ImageDraw.Draw(im)
+font = ImageFont.truetype('NotoSansCJKjp-Medium.otf', 16)
+draw.text((20, 0), li, fill=(25, 40, 100), font=font, spacing=8)
+im.save("image.png")
