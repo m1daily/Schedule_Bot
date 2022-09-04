e40a954eee8d2842484cd47473633b97efa7a141
Geusen <89836078+Geusen@users.noreply.github.com>
2022-09-05 07:28:39 +0900

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -14,7 +14,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from oauth2client.service_account import ServiceAccountCredentials
 
 
 #----------------------------------------------------------------------------------------------------
