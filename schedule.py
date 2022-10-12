fed975633b881617fb87350a32b5173eb3d11874
Geusen <89836078+Geusen@users.noreply.github.com>
2022-10-13 00:04:31 +0900

    {{important}}複数画像対応

--- schedule.py
+++ schedule.py
@@ -1,13 +1,12 @@
-import datetime
-import os
-import time
-import json
-import requests
-import urllib.request
-import cv2
-import numpy as np
-import tweepy
-from selenium import webdriver
+import datetime    # 日付取得
+import os    # 環境変数用
+import time    # 待機
+import cv2u    # 画像URLから読み込み
+import tweepy    # Twitter送信
+import requests    # LINE・Discord送信
+import json    # webhook用
+import urllib.request    # 画像取得
+from selenium import webdriver    # サイトから画像取得(以下略)
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -32,11 +31,11 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 # LINEの設定
-def line_notify(line_access_token):
+def line_notify(line_access_token, image):
     line_url = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': 'Bearer ' + line_access_token}
     payload = {'message': '時間割が更新されました。'}
-    files = {'imageFile': open('upload.png', 'rb')}
+    files = {'imageFile': open(image, 'rb')}
     r = requests.post(line_url, headers=headers, params=payload, files=files)
 
 # LINE,Discordのtoken設定(伏せています)
@@ -57,7 +56,7 @@ options.headless = True
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('chromedriver',options=options)
-driver.implicitly_wait(10)
+driver.implicitly_wait(5)
 
 # Googleスプレッドシートへ移動(URLは伏せています)
 driver.get(os.environ['GOOGLE_URL'])    # 時間割の画像があるGoogleSpreadSheetのURL
@@ -65,75 +64,91 @@ WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located)
 time.sleep(5)
 
 # imgタグを含むものを抽出
-li = driver.find_elements(By.TAG_NAME, 'img')
-if li == []:
+web_li = driver.find_elements(By.TAG_NAME, 'img')
+if web_li == []:
     finish('画像が発見できなかったため終了')
-
-# 時間割の画像以外も取り出している場合があるため、時間割の画像のみ抽出(GoogleSpreadSheet上の画像は画像URLの末尾が「alr=yes」)
-for e in li:
-    imgurl_n = e.get_attribute('src')
-    if imgurl_n != None and 'alr=yes' in imgurl_n == True:
-        break
+# 時間割の画像以外も取り出している場合があるため時間割の画像のみ抽出(GoogleSpreadSheet上の画像は画像URLの末尾が「alr=yes」)
+imgcv2u_n = []    # cv2u用リスト(現在)
+imgurl_n = []     # URLリスト(現在)
+for e in web_li:
+    imgurl = e.get_attribute('src')
+    # リストに既に同じ画像がない場合リストに追加
+    if 'alr=yes' in imgurl and bool(str(cv2u.urlread(imgurl)) in imgcv2u_n) == False:
+        imgcv2u_n.append(str(cv2u.urlread(imgurl)))
+        imgurl_n.append(imgurl)
 # 時間割の画像が見つからなかった場合は終了
-if imgurl_n == None:
+if imgurl_n == []:
     finish('画像が発見できなかったため終了')
+print(imgurl_n)
+print('\n----------------------------------------------------------------------------------------------------')
 
 #----------------------------------------------------------------------------------------------------
-# 旧時間割の画像URL取得
-f = open('url.txt', 'r')
-imgurl_b = f.read()
-f.close()
+# 最後に投稿した画像のリストを読み込み
+with open('url.txt', 'r') as f:
+    imgurl_b = f.read().split()    # URLリスト(過去)
+    f.close()
+print(imgurl_b)
+imgcv2u_b = []    # cv2u用リスト(過去)
+for e in imgurl_b:
+    imgcv2u_b.append(str(cv2u.urlread(e)))
+
+# 比較
+if len(imgurl_n) == len(imgurl_b):
+    if bool(set(imgcv2u_n) == set(imgcv2u_b)) == True:
+        finish('画像が一致した為、終了')
+    else:
+        print('画像が一致しないので続行')
+else:
+    print('画像の枚数が異なるので続行')
 
 #----------------------------------------------------------------------------------------------------
 # 画像URLを使って画像をダウンロード
-def download(url, name, when):
-    with urllib.request.urlopen(url) as web_file:
-        time.sleep(3)
+images = []    # 画像のファイル名用リスト
+for i in imgurl_n:
+    with urllib.request.urlopen(i) as web_file:
+        time.sleep(5)
         data = web_file.read()
-        with open(name, mode='wb') as local_file:
+        img = str(imgurl_n.index(i) + 1) + '.png'    # ファイル名を"リストの順番.png"に
+        images.append(img)
+        with open(img, mode='wb') as local_file:
             local_file.write(data)
-    print(when + ': ' + url)
 
-# 現在の時間割と旧時間割を比較
-download(imgurl_b, 'before.png', '前')
-download(imgurl_n, 'upload.png', '現在')
-img_1 = cv2.imread('before.png')
-img_2 = cv2.imread('upload.png')
-print('判定: ' + str(np.array_equal(img_1, img_2)))
+# 上書き
+with open('url.txt', 'w') as f:
+    f.write(' \n'.join(imgurl_n))
+    f.close()
 
 #----------------------------------------------------------------------------------------------------
-# 時間割の画像が一致しなかった(=時間割が更新されていた)場合
-if np.array_equal(img_1, img_2) == False:
-    
-    # url.txtに現在の画像のURLを上書き
-    f = open('url.txt', 'w')
-    f.write(imgurl_n)
-    f.close()
-    
-    # ツイート
-    api.update_status_with_media(status='時間割が更新されました！', filename='upload.png')
+# ツイート
+media_ids = []
+for image in images:
+   img = api.media_upload(image)
+   media_ids.append(img.media_id)
+# api.update_status(status='時間割が更新されました！', media_ids=media_ids)
 
-    # LINEへ通知
-    line_list = [notify_group, notify_27, notify_13]
-    for l in line_list:
-        line_notify(l)
-    
-    # DiscordのWebhookを通して通知
-    payload2 = {
-        'payload_json' : {
-            'content' : '@everyone\n時間割が更新されました。',
-            'embeds' : [
-                {
-                    'color' : 10931421,
-                    'image' : {'url' : imgurl_n}
-                }
-            ]
-        }
+# LINEへ通知
+'''
+line_list = [notify_group, notify_27, notify_13]    # 送信先のグループ
+for l in line_list:
+    for i in images:
+        line_notify(l, i)
+'''
+
+# DiscordのWebhookを通して通知
+payload2 = {
+    'payload_json' : {
+        'content' : '@everyone\n時間割が更新されました。',
     }
-    payload2['payload_json'] = json.dumps(payload2['payload_json'], ensure_ascii=False)
-    res = requests.post(webhook_url, data=payload2)
-    finish('投稿完了')
-
-# 時間割の画像が一致した(=時間割が更新されていなかった)場合
-else:
-    finish('画像が一致した為、終了')
+}
+embed = []
+# 画像の枚数分"embed"の値追加
+for i in imgurl_n:
+    if imgurl_n.index(i) == 0:
+        new_d = {'color' : 10931421, 'url' : 'https://www.google.com/','image' : {'url' : i}}
+    else:
+        new_d = {'url' : 'https://www.google.com/','image' : {'url' : i}}
+    embed.append(new_d)
+payload2['payload_json']['embeds'] = embed
+payload2['payload_json'] = json.dumps(payload2['payload_json'], ensure_ascii=False)
+# res = requests.post(webhook_url, data=payload2)
+finish('投稿完了')
