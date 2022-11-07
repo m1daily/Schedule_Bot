6d07f3026b8c5ab8e55c18a5f530531628cedff8
Geusen <89836078+Geusen@users.noreply.github.com>
2022-11-08 08:49:11 +0900

    test完了

--- schedule.py
+++ schedule.py
@@ -72,7 +72,7 @@ time.sleep(5)
 # imgタグを含むものを抽出
 imgs_tag = driver.find_elements(By.TAG_NAME, 'img')
 if imgs_tag == []:
-    finish('画像が発見できなかったため終了')
+    finish('画像が発見できなかったため終了1')
 # 時間割の画像以外も取り出している場合があるため時間割の画像のみ抽出(GoogleSpreadSheet上の画像は画像URLの末尾が「alr=yes」)
 imgs_cv2u_now = []    # cv2u用リスト(現在)
 imgs_url_now = []     # URLリスト(現在)
@@ -84,7 +84,7 @@ for e in imgs_tag:
         imgs_url_now.append(img_url)
 # 時間割の画像が見つからなかった場合は終了
 if imgs_url_now == []:
-    finish('画像が発見できなかったため終了')
+    finish('画像が発見できなかったため終了2')
 print(imgs_url_now)
 
 # $GITHUB_OUTPUTに追加
@@ -138,32 +138,32 @@ with open("README.md", mode='w', encoding='utf-8')as f:
     f.writelines(markdown_texts)
 
 #----------------------------------------------------------------------------------------------------
-# # ツイート
-# media_ids = []
-# for image in imgs_path:
-#    img = api.media_upload(image)
-#    media_ids.append(img.media_id)
-# api.update_status(status='時間割が更新されました！', media_ids=media_ids)
+# ツイート
+media_ids = []
+for image in imgs_path:
+   img = api.media_upload(image)
+   media_ids.append(img.media_id)
+api.update_status(status='時間割が更新されました！', media_ids=media_ids)
 
-# # LINEへ通知
-# line_list = [notify_group, notify_27, notify_13]    # 送信先のグループ
-# print('[LINE]')
-# for ll, line in enumerate(line_list, 1):
-#     for i, image in enumerate(imgs_path, 1):
-#         print(str(ll) + '-' + str(i) + ': ' + line_notify(line, image))
+# LINEへ通知
+line_list = [notify_group, notify_27, notify_13]    # 送信先のグループ
+print('[LINE]')
+for ll, line in enumerate(line_list, 1):
+    for i, image in enumerate(imgs_path, 1):
+        print(str(ll) + '-' + str(i) + ': ' + line_notify(line, image))
 
-# # DiscordのWebhookを通して通知
-# payload2 = {'payload_json' : {'content' : '@everyone\n時間割が更新されました。'}}
-# embed = []
-# # 画像の枚数分"embed"の値追加
-# for i in imgs_url_now:
-#     if imgs_url_now.index(i) == 0:
-#         img_embed = {'color' : 10931421, 'url' : 'https://www.google.com/', 'image' : {'url' : i}}
-#     else:
-#         img_embed = {'url' : 'https://www.google.com/', 'image' : {'url' : i}}
-#     embed.append(img_embed)
-# payload2['payload_json']['embeds'] = embed
-# payload2['payload_json'] = json.dumps(payload2['payload_json'], ensure_ascii=False)
-# res = requests.post(webhook_url, data=payload2)
-# print('Discord_Webhook: ' + str(res.status_code))
-# finish('投稿完了')
+# DiscordのWebhookを通して通知
+payload2 = {'payload_json' : {'content' : '@everyone\n時間割が更新されました。'}}
+embed = []
+# 画像の枚数分"embed"の値追加
+for i in imgs_url_now:
+    if imgs_url_now.index(i) == 0:
+        img_embed = {'color' : 10931421, 'url' : 'https://www.google.com/', 'image' : {'url' : i}}
+    else:
+        img_embed = {'url' : 'https://www.google.com/', 'image' : {'url' : i}}
+    embed.append(img_embed)
+payload2['payload_json']['embeds'] = embed
+payload2['payload_json'] = json.dumps(payload2['payload_json'], ensure_ascii=False)
+res = requests.post(webhook_url, data=payload2)
+print('Discord_Webhook: ' + str(res.status_code))
+finish('投稿完了')
