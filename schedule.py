f43cf9b203d71cbd1feb6112af8c4de881d36c60
Geusen <89836078+Geusen@users.noreply.github.com>
2022-12-08 23:10:42 +0900

    [fix]変数名ミス修正

--- schedule.py
+++ schedule.py
@@ -63,7 +63,7 @@ def finish(exit_message):
 # blob形式のURLの対策
 def get_blob_file(driver, url):
     result = driver.execute_async_script("""
-        var uri = arguments[0];
+        var url = arguments[0];
         var callback = arguments[1];
         var toBase64 = function(buffer){for(var r,n=new Uint8Array(buffer),t=n.length,a=new Uint8Array(4*Math.ceil(t/3)),i=new Uint8Array(64),o=0,c=0;64>c;++c)i[c]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".charCodeAt(c);for(c=0;t-t%3>c;c+=3,o+=4)r=n[c]<<16|n[c+1]<<8|n[c+2],a[o]=i[r>>18],a[o+1]=i[r>>12&63],a[o+2]=i[r>>6&63],a[o+3]=i[63&r];return t%3===1?(r=n[t-1],a[o]=i[r>>2],a[o+1]=i[r<<4&63],a[o+2]=61,a[o+3]=61):t%3===2&&(r=(n[t-2]<<8)+n[t-1],a[o]=i[r>>10],a[o+1]=i[r>>4&63],a[o+2]=i[r<<2&63],a[o+3]=61),new TextDecoder("ascii").decode(a)};
         var xhr = new XMLHttpRequest();
