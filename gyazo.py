63ef5ce60df121485a5048c2c08317ca31028c81
SoniPana <89836078+SoniPana@users.noreply.github.com>
2024-12-13 15:53:07 +0900

    [update]土曜課外のアップロード先をgyazoに変更

---
+++ gyazo.py
@@ -0,0 +1,19 @@
+import json
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+headers = {"Authorization": f"Bearer {os.environ['GYAZO']}"}
+files = {"imagedata": open("./sat.jpeg", "rb")}
+r = requests.post("https://upload.gyazo.com/api/upload", headers=headers, files=files)
+try:
+    r.raise_for_status()
+except requests.RequestException as e:
+    print("request failed. error=(%s)", e.response.text)
+    exit(1)
+url = json.loads(r.text)["url"]
+
+print(url)
